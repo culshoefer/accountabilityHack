bf36eb25b6930ead647c62b095d7ffe18b930b64
jermenkoo <latal.jaromir@gmail.com>
2015-11-21 12:55:12 +0000

    finished feedparser to get the links

--- feed_parser.py
+++ feed_parser.py
@@ -4,8 +4,13 @@ import xml.etree.ElementTree as ET
 FEED_URL = "http://api.data.parliament.uk/resources/files/feed?dataset=12"
 xml_response = r.get(FEED_URL)
 
-if r.status_code == 200:
-    tree = ET.parse(xml_response)
-    root = tree.getroot()
-
+if xml_response.status_code == 200:
+    root = ET.fromstring(xml_response.text)
     print(root)
+
+    for child in root:
+        for elem in child:
+            entry = elem.attrib
+
+            if entry.get('href') is not None:
+                print(entry['href'])
