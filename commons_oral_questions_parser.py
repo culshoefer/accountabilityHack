4c6243669cd8564391e9c7af8f7986223e627ece
jermenkoo <latal.jaromir@gmail.com>
2015-11-21 17:51:32 +0000

    edited synonym parser & created json file

--- commons_oral_questions_parser.py
+++ commons_oral_questions_parser.py
@@ -3,7 +3,7 @@ import requests
 
 # TODO: change for >500 results
 SEARCH_ENDPOINT_URL = "http://lda.data.parliament.uk/commonsoralquestions.json?_view=Commons+Oral+Questions&_pageSize=500&_search={}&_page=0"
-SYNONYM_ENDPOINT_URL = "http://lda.data.parliament.uk/terms.json?_view=Thesaurus&_pageSize=500&_search=%22{}%22&_page=0&_sort=attribute"
+SYNONYM_ENDPOINT_URL = "http://lda.data.parliament.uk/terms.json?_view=Thesaurus&_pageSize=500&_search=%22{}%22&_page=0&_properties=prefLabel,exactMatch.prefLabel"
 
 links_to_articles = {}
 
@@ -21,6 +21,7 @@ def get_synonyms(word):
         most_exact = response_json['result']['items'][0]
         # print('Most exact match:', most_exact)
         for match in most_exact['exactMatch']:
+            # print(match)
             # if the match is not an URL
             if isinstance(match, dict):
                 synonyms.append(match['prefLabel']['_value'])
@@ -58,11 +59,32 @@ def search_all():
             print("Searching for:", word)
             search_articles(word, SEARCH_ENDPOINT_URL)
 
-print("\n[*]get_synonyms")
-print(get_synonyms("tax"))
+
+def get_SYN(word):
+    synonyms = set()
+
+    full_query = SYNONYM_ENDPOINT_URL.format(word)
+    response = requests.get(full_query)
+
+    if response.status_code == 200:
+        response_json = json.loads(response.text)
+
+        most_exact = response_json['result']['items']
+        for match in most_exact:
+            # print(match["prefLabel"])
+            # print(match["exactMatch"]["prefLabel"]["_value"])
+            # if isinstance(match.get("exactMatch"), dict):
+            exact_match = match.get("exactMatch")
+            # print(match["prefLabel"]["_value"])
+
+    return synonyms
+
+# print("\n[*]get_synonyms")
+# print(get_SYN("tax"))
 
 # print("\n[*]search_articles")
-# search_articles("Islamic State", SEARCH_ENDPOINT_URL)
+search_articles("Islamic State", SEARCH_ENDPOINT_URL)
+print(links_to_articles)
 
 # print("\n[*]search_all")
 # search_all()
