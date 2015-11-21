1365cdede62784d02cf34ed062c6a97982ef4a62
culshoefer <christoph.ulshoefer.15@ucl.ac.uk>
2015-11-21 22:05:23 +0000

    Treemap is now full screen
    Todo: Let fullscreen be flexible

--- script.js
+++ script.js
@@ -11,22 +11,31 @@ var tree = {
     ]
 };
 
-function render() {
-    var margin = {top: 40, right: 10, bottom: 10, left: 10},
-        width = 960 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
+function switchData(){
 
+}
+
+function render() {
+    var margin = {top: 0, right: 0, bottom: 0, left: 0},//top: 40, right: 10, bottom: 10, left: 10},
+        width = window.innerWidth, //parseInt(d3.select("#treeDiv").style('width'), 10), //960 - margin.left - margin.right,
+        height = window.innerHeight; //parseInt(d3.select("#treeDiv").style('height'), 10); //500 - margin.top - margin
+	
     var color = d3.scale.category10()
     var treemap = d3.layout.treemap()
         .size([width, height])
         .sticky(true)
-        .value(function value(d){
-	    return d.mentions;
+        .value(function value(d, depth){
+	    if (depth == 1)
+		return d.mentions;
+	    if (depth == 2)
+		return d.links.length;
 	});
 
     treemap.children(function children(d, depth){
 	if (depth == 0)
 	    return d.topics;
+	else if (depth == 1)
+	    return d.MPs;
     });
 
     var div = d3.select("#treeDiv").append("div");
@@ -45,6 +54,9 @@ function render() {
 
     });
 
+/*    treemap.enter()
+	.append()
+    treemap.on("click*/
     function position() {
         this.style("left", function(d) { return d.x + "px"; })
             .style("top", function(d) { return d.y + "px"; })
